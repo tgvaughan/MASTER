b00d561fdd24bd1da6514ad85281f90bcde83780
Tim Vaughan <tgvaughan@gmail.com>
2012-03-06 16:52:21 +1300

    Added EnsembleSummary class.

---
+++ src/viralPopGen/EnsembleSummary.java
@@ -0,0 +1,25 @@
+package viralPopGen;
+
+import java.util.*;
+
+/**
+ * A class representing a collection of results obtained by
+ * estimating moments from an ensemble of trajectories of
+ * a birth-death process.
+ * 
+ * @author tvaughan
+ *
+ */
+public class EnsembleSummary {
+	
+	ArrayList<Moment> moments;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

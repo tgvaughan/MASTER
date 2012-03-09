99bc5895386f4d4a31eafcfc5cd9c8d09b66f993
Tim Vaughan <tgvaughan@gmail.com>
2012-03-09 18:28:24 +1300

    Got rid of scalar populations from State methods.

--- src/viralPopGen/State.java
+++ src/viralPopGen/State.java
@@ -12,8 +12,7 @@ import java.util.*;
 public class State {
 	
 	Model model;
-	HashMap<Population,Double[]> genPopSizes;
-	HashMap<Population,Double> scalarPopSizes;
+	HashMap<Population,Double[]> popSizes;
 	
 	
 	/**
@@ -25,13 +24,10 @@ public class State {
 		
 		this.model = model;
 		
-		// Initialise genPopSizes and scalarPopSizes:
-		
-		genPopSizes = new HashMap<Population, Double[]>(model.geneticPops.size());
+		// Initialise sub-population sizes:
+		popSizes = new HashMap<Population, Double[]>(model.geneticPops.size());
 		for (Population p : model.geneticPops)
-			genPopSizes.put(p, new Double[model.typeNum]);
-		
-		scalarPopSizes = new HashMap<Population, Double>(model.scalarPops.size());
+			popSizes.put(p, new Double[model.typeNum]);
 		
 	}
 	
@@ -43,18 +39,14 @@ public class State {
 	public State (State oldState) {
 		this.model = oldState.model;
 		
-		// Copy genetic population sizes:
-		this.genPopSizes = new HashMap<Population, Double[]>(model.geneticPops.size());
+		// Copy sub-population sizes:
+		this.popSizes = new HashMap<Population, Double[]>(model.geneticPops.size());
 		for (Population p : model.geneticPops) {
-				genPopSizes.put(p, new Double[model.typeNum]);
+				popSizes.put(p, new Double[model.typeNum]);
 				for (int i=0; i<model.typeNum; i++)
-					genPopSizes.get(p)[i] = oldState.genPopSizes.get(p)[i];
+					popSizes.get(p)[i] = oldState.popSizes.get(p)[i];
 		}
 		
-		// Copy scalar population sizes:
-		scalarPopSizes = new HashMap<Population, Double>(model.scalarPops.size());
-		for (Population p : model.scalarPops)
-			scalarPopSizes.put(p, oldState.getScalar(p));
 	}
 	
 	/**
@@ -64,113 +56,44 @@ public class State {
 	 */
 	public void makeCopy (State oldState) {
 		
-		// Copy genetic population sizes:
+		// Copy sub-population sizes:
 		for (Population p : model.geneticPops) {
 			for (int i=0; i<model.typeNum; i++)
-				genPopSizes.get(p)[i] = oldState.genPopSizes.get(p)[i];
+				popSizes.get(p)[i] = oldState.popSizes.get(p)[i];
 		}
-		
-		// Copy scalar population sizes:
-		for (Population p : model.scalarPops)
-			scalarPopSizes.put(p,  oldState.getScalar(p));
 		
 	}
 
 	/**
-	 * Get size of a particular genetic sub-population.
+	 * Get size of a particular sub-population.
 	 * 
 	 * @param p		Population to interrogate.
 	 * @param loc	Specific sub-population location.
 	 * @return Size of sub-population.
 	 */
-	public double getGen(Population p, int[] loc) {
-		return genPopSizes.get(p)[model.locToOffset(loc)];
+	public double get(Population p, int[] loc) {
+		return popSizes.get(p)[model.locToOffset(loc)];
 	}
 	
 	/**
-	 * Set size of a particular genetic sub-population.
+	 * Set size of a particular sub-population.
 	 * 
 	 * @param p		Population to modify.
 	 * @param loc	Specific sub-population location.
 	 * @param value	Desired size.
 	 */
-	public void setGen(Population p, int[] loc, double value) {
-		genPopSizes.get(p)[model.locToOffset(loc)] = value;
-	}
-	
-	/**
-	 * Get size of particular scalar population.
-	 * 
-	 * @param p	Population to interrogate.
-	 * @return Size of population.
-	 */
-	public double getScalar(Population p) {
-		return scalarPopSizes.get(p);
-	}
-	
-	/**
-	 * Set size of a scalar population.
-	 * 
-	 * @param p			Population to modify.
-	 * @param value		Desired size.
-	 */
-	public void setScalar(Population p, double value) {
-		scalarPopSizes.put(p, value);
+	public void set(Population p, int[] loc, double value) {
+		popSizes.get(p)[model.locToOffset(loc)] = value;
 	}
 
-	/**
-	 * Add two states together.
-	 * 
-	 * @param arg	State to add.
-	 * @return		Result of addition.
-	 */
-	public State add(State arg) {
-
-		for (Population p : genPopSizes.keySet()) {
-			for (int i=0; i<genPopSizes.get(p).length; i++)
-				genPopSizes.get(p)[i] += arg.genPopSizes.get(p)[i];
-		}
-		
-		for (Population p : scalarPopSizes.keySet()) {
-			double thisSize = scalarPopSizes.get(p);
-			scalarPopSizes.put(p, thisSize + arg.scalarPopSizes.get(p));
-		}
-
-		return this;
-	}
-
-	/**
-	 * Multiply state by an integer.
-	 * 
-	 * @param arg	Integer to multiply.
-	 * @return		Result of multiplication.
-	 */
-	public State mul(int arg) {
-		
-		for (Population p : genPopSizes.keySet()) {
-			for (int i=0; i<genPopSizes.get(p).length; i++)
-				genPopSizes.get(p)[i] *= arg;
-		}
-		
-		for (Population p : scalarPopSizes.keySet()) {
-			double thisSize = scalarPopSizes.get(p);
-			scalarPopSizes.put(p, thisSize*arg);
-		}
-
-		return this;
-	}
-	
 	/**
 	 * Dump representation of state to stdout.
 	 */
 	public void dump() {
 
-		for (Population p : scalarPopSizes.keySet())
-			System.out.print(" " + String.valueOf(scalarPopSizes.get(p)));
-
-		for (Population p : genPopSizes.keySet()) {
-			for (int i=0; i<genPopSizes.get(p).length; i++)
-				System.out.print(" " + String.valueOf(genPopSizes.get(p)[i]));
+		for (Population p : popSizes.keySet()) {
+			for (int i=0; i<popSizes.get(p).length; i++)
+				System.out.print(" " + String.valueOf(popSizes.get(p)[i]));
 		}
 
 		System.out.println();
@@ -182,11 +105,8 @@ public class State {
 	 */
 	public void dumpNames() {
 		
-		for (Population p : scalarPopSizes.keySet())
-			System.out.print(" " + p.name);
-
-		for (Population p : genPopSizes.keySet()) {
-			for (int i=0; i<genPopSizes.get(p).length; i++)
+		for (Population p : popSizes.keySet()) {
+			for (int i=0; i<popSizes.get(p).length; i++)
 				System.out.print(" " + p.name + String.valueOf(i));
 		}
 		

899df94f2282b53f53c6b960f671054ba89a5113
Tim Vaughan <tgvaughan@gmail.com>
2012-03-13 16:46:10 +1300

    Started work on Moment class.

--- src/viralPopGen/Moment.java
+++ src/viralPopGen/Moment.java
@@ -1,18 +1,24 @@
 package viralPopGen;
 
+import java.util.*;
+
 /**
- * Class to extend to construct classes of objects
- * tailored to calculating particular moments.
+ * Class of objects representing moments to be estimated from
+ * system state ensemble.  Very similar to Reaction class in design.
  * 
  * @author Tim Vaughan
  *
  */
 public class Moment {
 	
+	// Name of moment - used in output file:
 	String name;
-	int[] dims;
-	double []data;
-	int n;
+	
+	// Specification of moment:
+	HashMap<Population,ArrayList<HashMap<Integer,Integer>>> components;
+	
+	// Estimates:
+	double[] mean, var;
 	
 	/**
 	 * Record sample of moment calculated from state.

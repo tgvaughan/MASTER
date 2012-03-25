3dd6ca678236f6bfe12b425525d7e4664ab06b3e
Tim Vaughan <tgvaughan@gmail.com>
2012-03-26 12:12:17 +1300

    Poissonian RNG now uses BEAST's lnGamma function.

--- src/viralPopGen/math/Poisson.java
+++ src/viralPopGen/math/Poisson.java
@@ -1,70 +1,49 @@
 package viralPopGen.math;
 
 import beast.util.Randomizer;
+import beast.math.GammaFunction;
 
 /**
  * Class with a static method useful for generating sequences
  * of numbers which appear to be chosen from Poissonian distributions.
  * 
- * Uses BEAST's RNG.
+ * Uses BEAST's uniform RNG and logGamma.
+ * 
+ * Note that the nextDouble(lambda) method implemented by this class
+ * produces sensible results even when lambda exceeds Integer.MAXINT.
  * 
  * @author Tim Vaughan
  *
  */
 public class Poisson {
 
-	private static double PI = 3.141592653589793238;
-
-	// If you believe NR, returns the value ln[Gamma(xx)] for xx>0. Pure black magick.
-	private static double gammln(double xx)
-	{
-		int j;
-		double x,y,tmp,ser;
-		double [] cof = {
-				76.18009172947146,
-				-86.50532032941677,
-				24.01409824083091,
-				-1.231739572450155,
-				0.1208650973866179e-2,
-				-0.5395239384953e-5
-				};
-
-		y = x = xx;
-
-		tmp = x + 5.5;
-		tmp -= (x + 0.5)*Math.log(tmp);
-
-		ser=1.000000000190015;
-
-		for (j=0; j<6; j++)
-			ser += cof[j]/++y;
-
-		return -tmp + Math.log(2.5066282746310005*ser/x);
-	}
+	// If mean is above this, use rejection sampling method.
+	private static double REJECT = 12;
 
 	// Rejection method from NR, apparently good for lambda>=12
 	private static double poissonian_reject(double lambda)
 	{
 		double sq = Math.sqrt(2.0*lambda);
 		double alxm = Math.log(lambda);
-		double g = lambda*alxm - gammln(lambda+1.0);
+		double g = lambda*alxm - GammaFunction.lnGamma(lambda+1.0);
 		double em, t, y;
 
 		do {
 			do {
-				y = Math.tan(PI*Randomizer.nextDouble());
+				y = Math.tan(Math.PI*Randomizer.nextDouble());
 				em = sq*y + lambda;
 			} while (em < 0.0);
 
 			em = Math.floor(em);
-			t = 0.9*(1.0 + y*y)*Math.exp(em*alxm - gammln(em + 1.0)-g);
+			t = 0.9*(1.0 + y*y)*Math.exp(em*alxm -
+				GammaFunction.lnGamma(em + 1.0)-g);
 
 		} while (Randomizer.nextDouble() > t);
 
 		return em;
 	}
 
-	// Direct method due to Knuth. Only efficient for small lambda. Bad Knuth.
+	// Direct method due to Knuth. Only efficient for small lambda.
 	private static double poissonian_knuth(double lambda)
 	{
 		double L = Math.exp(-lambda);
@@ -80,10 +59,26 @@ public class Poisson {
 	// Meta-function selects direct or rejection method according to lambda.
 	public static double nextDouble(double lambda)
 	{
-		if (lambda < 12.0)
+		if (lambda < REJECT)
 			return poissonian_knuth(lambda);
 
 		return poissonian_reject(lambda);
+	}
+
+	/**
+	 * Speed test.
+	 * 
+	 * @param argv 
+	 */
+	public static void main (String[] argv) {
+
+		double res = 0;
+		for (int i=0; i<100000; i++)
+			for (int j=0; j<100; j++)
+				res = nextDouble(1e10);
+
+		System.out.println(res);
+
 	}
 
 }

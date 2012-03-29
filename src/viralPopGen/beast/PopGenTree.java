3c26f09a4e43cbf7c118f4a2ca8a2180b2e32a67
Tim Vaughan <tgvaughan@gmail.com>
2012-03-30 10:42:49 +1300

    New class to draw trees from trajectories in BEAST.

---
+++ src/viralPopGen/beast/PopGenTree.java
@@ -0,0 +1,13 @@
+package viralPopGen.beast;
+
+import beast.evolution.tree.*;
+
+/**
+ * BEAST 2 plugin to enable trajectories to be represented
+ * as ancestral trees.
+ *
+ * @author Tim Vaughan
+ */
+public class PopGenTree extends Tree {
+	
+}

06c791d40a03bb623e67bee0d1323743ae849f85
Tim Vaughan <tgvaughan@gmail.com>
2012-12-25 18:30:29 +1300

    Minor change to deployment script.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -15,7 +15,7 @@ mkdir $ojdir/lib
 cp ~/work/beast2/dist/beast2.jar $ojdir/main
 cp ~/work/beast2/lib/*.jar $ojdir/lib
 cp ~/work/MASTER/dist/MASTER.jar $ojdir/lib
-cp ~/work/MASTER/dist/lib/*.jar $ojdir/lib
+cp ~/work/MASTER/lib/*.jar $ojdir/lib
 
 cp one-jar-boot-0.97.jar $ojdir/
 cd $ojdir

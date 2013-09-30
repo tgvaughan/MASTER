327e18bed84ea4f4b0fa94d166c593c6b009e833
Tim Vaughan <tgvaughan@gmail.com>
2013-10-01 11:10:20 +1300

    Updated package creation script.

--- deployment/makePackage.sh
+++ deployment/makePackage.sh
@@ -3,37 +3,43 @@
 # Assemble BEAST 2 package
 #
 
-PKGNAME=MASTER
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 version"
+    exit
+fi
 
-rm -rf $PKGNAME $PKGNAME.zip
-mkdir $PKGNAME
-mkdir $PKGNAME/examples
-mkdir $PKGNAME/lib
-mkdir $PKGNAME/doc
+PKGNAME=MASTER
+VERSION=$1
+
+FULLNAME=$PKGNAME-$VERSION
+rm -rf $FULLNAME $FULLNAME.zip
+mkdir $FULLNAME
+mkdir $FULLNAME/examples
+mkdir $FULLNAME/lib
+mkdir $FULLNAME/doc
 
 # Create source jar file
 pushd ..
-jar cf $PKGNAME.src.jar src test
+jar cf $FULLNAME.src.jar src test
 popd
-mv ../$PKGNAME.src.jar $PKGNAME
+mv ../$FULLNAME.src.jar $FULLNAME
 
 # Copy over examples
-cp ../examples/*.xml $PKGNAME/examples
+cp ../examples/*.xml $FULLNAME/examples
 
 # Copy over binaries
-cp ../dist/$PKGNAME.jar $PKGNAME/lib
-cp ../lib/*.jar $PKGNAME/lib
+cp ../dist/$PKGNAME.jar $FULLNAME/lib
+cp ../lib/*.jar $FULLNAME/lib
 
 # Copy over documentation
 
 # Create version.xml
-cat <<EOF > $PKGNAME/version.xml
-<addon name="$PKGNAME" version="1.4.2">
+cat <<EOF > $FULLNAME/version.xml
+<addon name="$PKGNAME" version="$VERSION">
     <depends on="beast2" atleast="2.0.2"/>
 </addon>
-
 EOF
 
 # Create archive and clean up
-zip -r $PKGNAME.zip $PKGNAME
-rm -rf $PKGNAME
+zip -r $FULLNAME.zip $FULLNAME
+rm -rf $FULLNAME
